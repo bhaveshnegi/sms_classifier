2859e706078c8465f506385f96c108d6ac148e22
bhaveshnegi <bhaveshnegi98@gmail.com>
2023-12-11 23:16:19 +0530

    preprocess

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ import streamlit as st
 import pickle
 import string
 from nltk.corpus import stopwords
+import nltk
+from nltk.stem.porter import PorterStemmer
+
+ps = PorterStemmer()
+
 
 def transform_text(text):
     text = text.lower()
@@ -26,12 +31,23 @@ def transform_text(text):
     
     return " ".join(y)
 
-tfidf = pickle.load(open('vectorizer.pkl','rb'))
-model = pickle.load(open('model.pkl','rb'))
+tfidf = pickle.load(open('C:/Users/bhave/OneDrive/Desktop/Github/sms_classifier/vectorizer.pkl', 'rb'))
+
+model = pickle.load(open('C:/Users/bhave/OneDrive/Desktop/Github/sms_classifier/model.pkl','rb'))
 
 st.title("SMS Classifier")
-int_sms = st.text_input("Enter the Message")
+int_sms = st.text_area("Enter the Message")
 
-#preprocess
-transform_sms = transform_text(int_sms)
+if st.button("Predict"):
+    # preprocess
+    transformed_sms = transform_text(int_sms)
+    # vectorize
+    vector_input = tfidf.transform([transformed_sms])
+    # predict
+    result = model.predict(vector_input)[0]
+    # display
+    if result==1:
+        st.header("It is Spam Message!")
+    else:
+        st.header("It is Not A Spam Message!")
 
